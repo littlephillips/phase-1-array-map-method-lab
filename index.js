cb94511bd34c23146d971a87dad2d73377a941b6
littlephillips <phillipkinuthia72@gmail.com>
2022-06-11 23:48:06 +0300

    modified index.js

--- index.js
+++ index.js
@@ -12,5 +12,12 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map((list) =>  {
+    const title = list.split( ` `);
+    const capitalizedTitle = title.map(
+      (title) => title.charAt(0).toUpperCase() + title.slice(1)
+    );
+    const result = capitalizedTitle.join(` `);
+    return result;
+  });
+};
